0c16216cbea68ead466e26920b1e6a5f8ccfc092
cyantis <cyantis@gmail.com>
2019-07-19 16:59:47 -0600

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,10 +1,6 @@
 class Admin::PreferencesController < ApplicationController
   def index
-    @preference = Preference.new
-  end
-
-  def create
-    @preferenc = Preference.create(preference_params)
+    @preference = Preference.first_or_create(:artist_sort_order => "ASC", :song_sort_order => "ASC", :allow_create_artists => true, :allow_create_songs => true)
   end
 
   def update
